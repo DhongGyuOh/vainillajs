7cc5a0ab1d2744ab382bec2ab23104a55545d3fd
DhongGyuOh <45752420+DhongGyuOh@users.noreply.github.com>
2023-05-15 17:32:55 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+let player = {
+  name: "DongGyu",
+  age: 32,
+};
+
+const GetPlayer = {
+  names: function (str) {
+    console.log("My Name Is " + str);
+  },
+  ages: function (str) {
+    console.log("I'm " + str + " Years Old");
+  },
+  krage: function (age) {
+    return age + 2;
+  },
+};
+GetPlayer.names(player.name);
+GetPlayer.ages(player.age);
+const korage = GetPlayer.krage(30);
+console.log(korage);
+
+const userName = prompt("What's Your Name?");
+const userAge = parseInt(prompt("How Old Are You?"));
+console.log(isNaN(userAge));
+if (isNaN(userAge)) {
+  alert("나이가 잘못 되었습니다.");
+} else if (userAge == 100) {
+  alert("wise");
+}
+console.log(userName);
+console.log(userAge);
