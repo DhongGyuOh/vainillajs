7a4b3fc4f34d65378163a93a06ace465a9f002e3
DhongGyuOh <45752420+DhongGyuOh@users.noreply.github.com>
2023-05-16 15:54:24 +0900

    HTML Document

--- app.js
+++ app.js
@@ -19,13 +19,24 @@ GetPlayer.ages(player.age);
 const korage = GetPlayer.krage(30);
 console.log(korage);
 
-const userName = prompt("What's Your Name?");
-const userAge = parseInt(prompt("How Old Are You?"));
-console.log(isNaN(userAge));
-if (isNaN(userAge)) {
-  alert("나이가 잘못 되었습니다.");
-} else if (userAge == 100) {
-  alert("wise");
-}
-console.log(userName);
-console.log(userAge);
+// const userName = prompt("What's Your Name?");
+// const userAge = parseInt(prompt("How Old Are You?"));
+// console.log(isNaN(userAge));
+// if (isNaN(userAge)) {
+//   alert("나이가 잘못 되었습니다.");
+// } else if (userAge === 100) {
+//   alert("wise");
+// }
+// console.log(userName);
+// console.log(userAge);
+
+const title = document.getElementById("title");
+console.dir(title);
+
+title.innerText = "HI";
+
+const hellos = document.getElementsByClassName("hh");
+console.log(hellos);
+
+const htitle = document.querySelector(".hclass h1");
+console.log(htitle);
