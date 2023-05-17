277431ab0f717883b43c60486c4bf59386c45ff0
DhongGyuOh <45752420+DhongGyuOh@users.noreply.github.com>
2023-05-17 11:40:30 +0900

    EventListener

--- app.js
+++ app.js
@@ -1,42 +1,27 @@
-let player = {
-  name: "DongGyu",
-  age: 32,
-};
-
-const GetPlayer = {
-  names: function (str) {
-    console.log("My Name Is " + str);
-  },
-  ages: function (str) {
-    console.log("I'm " + str + " Years Old");
-  },
-  krage: function (age) {
-    return age + 2;
-  },
-};
-GetPlayer.names(player.name);
-GetPlayer.ages(player.age);
-const korage = GetPlayer.krage(30);
-console.log(korage);
-
-// const userName = prompt("What's Your Name?");
-// const userAge = parseInt(prompt("How Old Are You?"));
-// console.log(isNaN(userAge));
-// if (isNaN(userAge)) {
-//   alert("나이가 잘못 되었습니다.");
-// } else if (userAge === 100) {
-//   alert("wise");
-// }
-// console.log(userName);
-// console.log(userAge);
-
-const title = document.getElementById("title");
+const title = document.querySelector(".main__title h1");
 console.dir(title);
 
-title.innerText = "HI";
+function handleTitle() {
+  title.style.color = "blue";
+  title.innerText = "OK";
+}
 
-const hellos = document.getElementsByClassName("hh");
-console.log(hellos);
-
-const htitle = document.querySelector(".hclass h1");
-console.log(htitle);
+function handleTitle2() {
+  title.style.color = "red";
+  title.innerText = "Are you sure?";
+}
+function handleTitle3() {
+  title.style.color = "black";
+  title.innerText = "Click Me!";
+}
+function handleResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+function handleCopy() {
+  alert("don't do that");
+}
+title.addEventListener("mouseleave", handleTitle3);
+title.addEventListener("mouseenter", handleTitle2);
+title.onclick = handleTitle;
+window.addEventListener("resize", handleResize);
+window.addEventListener("copy", handleCopy);
